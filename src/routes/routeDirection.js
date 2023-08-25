13cf66a4b4b1e691b34e4cb48caf2ec15232b66a
pawanagra <139465715+pawanagra@users.noreply.github.com>
2023-08-25 18:31:59 +0530

    Update routeDirection.js

--- src/routes/routeDirection.js
+++ src/routes/routeDirection.js
@@ -15,7 +15,7 @@ const express = require('express');
 const router = express.Router();
 const {updatestopDetailsByETAandTotalDistance_OtherPlannedRoutes} = require('../service/directions');
 const {updatestopDetailsByETAandTotalDistance_PlannedRoutes} = require('../service/directions');
-const {CalculateTraveTimeAndTravelDistance} = require('../service/directions');
+const {calculateTravelTimeAndTravelDistance} = require('../service/directions');
 const {processRoutesForDirections} = require('../service/directions');
 const authenticate = require('../authentication/auth')
 const { logger } = require('../../Logger/logger');
@@ -28,8 +28,8 @@ router.post('', authenticate, async (req, res) => {
   // Get the routes from the request body
   let routes = req.body;
   try {
-      // Use Promise.all to fetch directions for multiple planned_routes asynchronously
-      const planned_routes_results = await Promise.all(routes.planned_routes.map(async (plannedRoute) => {
+    // Use Promise.all to fetch directions for multiple planned_routes asynchronously
+    const planned_routes_results = await Promise.all(routes.planned_routes.map(async (plannedRoute) => {
       try {
         const result = await processRoutesForDirections(plannedRoute);
         return result;
@@ -37,10 +37,11 @@ router.post('', authenticate, async (req, res) => {
         return { status: 500, data: [] };
       }
     }));
-    
-      const other_planned_routes_results = await Promise.all(routes.other_planned_routes.map(async (plannedRoute) => {
+     
+    // Use Promise.all to fetch directions for multiple other_planned_routes asynchronously
+    const other_planned_routes_results = await Promise.all(routes.other_planned_routes.map(async (plannedRoute) => {
       try {
-        const result = await fetchGoogleDirectionsUsingUrlWithRetry(plannedRoute);
+        const result = await processRoutesForDirections(plannedRoute);
         return result;
       } catch (error) {
         return { status: 500, data: [] };
@@ -51,10 +52,10 @@ router.post('', authenticate, async (req, res) => {
     // by adding (ETA & ETD)
     for(let index = 0; index < routes.planned_routes.length; index++) {
       updatestopDetailsByETAandTotalDistance_PlannedRoutes(routes, planned_routes_results, routes.planned_routes[index].routeStartTime, 0, index);
- 
-      if(planned_routes_results[index].data && planned_routes_results[index].data.routes.length > 0) {  
 
-        let { travelTime, travelDistance } = CalculateTraveTimeAndTravelDistance(planned_routes_results[index]); // convert travelTime in HH:MM
+      if(Object.keys(planned_routes_results[index].data).length !== 0 && planned_routes_results[index].data.routes.length > 0) {  
+
+        let { travelTime, travelDistance } = calculateTravelTimeAndTravelDistance(planned_routes_results[index]); // convert travelTime in HH:MM
 
       // Converting travelDistance from meters to miles as we need it in miles  
         travelDistance *= (0.00062137119);
@@ -65,11 +66,13 @@ router.post('', authenticate, async (req, res) => {
       }
     }
     
+    // Loop through the results and update each other_planned_routes by adding (travelTime & travelDistance) and stopDetails at each stop(stopSequence) level
+    // by adding (ETA & ETD)
     for(let index = 0; index < routes.other_planned_routes.length; index++) {
       updatestopDetailsByETAandTotalDistance_OtherPlannedRoutes(routes, other_planned_routes_results, routes.other_planned_routes[index].routeStartTime, 0, index);
 
-      if(other_planned_routes_results[index].data && other_planned_routes_results[index].data.routes.length > 0) {  
-        let { travelTime, travelDistance } = CalculateTraveTimeAndTravelDistance(other_planned_routes_results[index]); // convert travelTime in HH:MM
+      if(Object.keys(other_planned_routes_results[index].data).length !== 0 && other_planned_routes_results[index].data.routes.length > 0) {  
+        let { travelTime, travelDistance } = calculateTravelTimeAndTravelDistance(other_planned_routes_results[index]); // convert travelTime in HH:MM
       // Converting travelDistance from meters to miles as we need it in miles  
         travelDistance *= (0.00062137119);
         
@@ -78,14 +81,13 @@ router.post('', authenticate, async (req, res) => {
         routes.other_planned_routes[index].travelDistance = travelDistance;
       }
     }
+
     return res.json({ status: 200, data: routes});
   } catch (error) {
-    // res.json({error: error.message})
     // Adding the logger for the error
     logger.error('router', 'routeDirection', error.message, '');
     console.log('inside the catch block')
     return res.json({ status: 500, data: routes})
-    // res.json({ status: 500, data: [] });
   }
 });
 
